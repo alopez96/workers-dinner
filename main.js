914fb47f13ab9c244143f8cbe62765a7acbd3baa
Arturo <alopez96@ucsc.edu>
2018-12-22 09:35:58 -0800

    button actions qork on safari

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 
 $(document).ready(function(){
     $("#about-btn").click(function(){
-        console.log('go to about')
       var id = "#" + $(this).attr('goto');
+      console.log($(this).attr('goto'))
       var top = $(id).position().top;
-      
-      $('html').scrollTop(top);
+      $('html, body').scrollTop(top);
+      console.log('go to about')
     });
-  });
 
-
-$(document).ready(function(){
-$("#org-btn").click(function(){
-    console.log('go to team')
+  $("#org-btn").click(function(){ 
     var id = "#" + $(this).attr('goto');
     var top = $(id).position().top;
-    
-    $('html').scrollTop(top);
+    $('html, body').scrollTop(top);
+    console.log('go to team')
+  });
+  
 });
-});
+
+
